e8a4121c214b25ea7bf3e658c815c2342c5867e8
Touheed Naveed <braja7059@gmail.com>
2024-09-14 02:40:39 +0500

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,18 @@
+import { Box, Stack, Typography } from '@mui/material'
 import React from 'react'
-
+import Logo from '../assets/images/logo3.png'
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <Box mt="80px" bgcolor="#fff3f4">
+
+      <Stack gap={"20px"} px={"40px"} pt={"24px"} style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+        <img src={Logo} alt="logo" width={"30px"} height={"30px"} />
+        <Typography variant='h6'>
+          Copyright © {new Date().getFullYear()} Touheed Naveed
+        </Typography>
+      </Stack>
+
+    </Box>
   )
 }
 
