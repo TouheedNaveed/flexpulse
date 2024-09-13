807081a10dc609cffc4c0d3e6d41dae1c19e107f
Touheed Naveed <braja7059@gmail.com>
2024-09-14 02:49:03 +0500

    fix details section

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -4,13 +4,13 @@ import { useParams } from 'react-router-dom';
 
 import {exerciseOptions,fetchData, youtubeOptions} from '../utils/fetchData'
 import Detail from '../components/Detail';
-import ExerciseVideos from '../components/ExerciseVideos';
+// import ExerciseVideos from '../components/ExerciseVideos';
 import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
 
   const [exerciseDetail,setExerciseDetail]=useState({});
-  const [exerciseVideos,setExerciseVideos]=useState([]);
+  // const [exerciseVideos,setExerciseVideos]=useState([]);
   const [targetMuscleExercises,setTargetMuscleExercises]=useState([]);
   const [equipmentExercises,setEquipmentExercises]=useState([]);
   const {id}=useParams();
@@ -24,9 +24,9 @@ const ExerciseDetail = () => {
       //setting exercise details to exercise detail data
       setExerciseDetail(exerciseDetailData);
 
-      //fetching youtube related data
-      const exerciseVideosData=await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetail.name}`,youtubeOptions);
-      setExerciseVideos(exerciseVideosData.contents);
+      // //fetching youtube related data
+      // const exerciseVideosData=await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetail.name}`,youtubeOptions);
+      // setExerciseVideos(exerciseVideosData.contents);
       //Target muscle data for similar exercises API call
       const targetMuscleExercisesData=await fetchData(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`,exerciseOptions);
       setTargetMuscleExercises(targetMuscleExercisesData);
